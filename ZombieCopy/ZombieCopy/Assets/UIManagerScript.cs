573698455fe1aaeaa4707fd2154998a5263dc632
jlawrey <j_lawrey@hotmail.com>
2014-11-25 20:29:04 -0500

    STN
    Timer Fix

---
+++ ZombieCopy/ZombieCopy/Assets/UIManagerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManagerScript : MonoBehaviour {
+	
+
+	void Start()
+	{
+		Screen.showCursor = true;		
+	}
+
+	public void StartGame()
+	{
+		Application.LoadLevel("SurviveTheNight");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+
+
+
+}
+
