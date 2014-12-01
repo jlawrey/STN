e12bbd079429840ea53dbc7734a1df550cf1d414
Jeremy Martin <jeremymartin1391@hotmail.com>
2014-12-01 16:02:04 -0500

    Main Menu's Working
    Play Game button will now start the game, revamped the main menu with
    more interesting decorations.

---
+++ ZombieCopy/Assets/JM_MainMenuScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JM_MainMenuScript : MonoBehaviour 
+{
+	public bool isQuit = false; //Only check true on the exit button, true means that button leaves the game
+
+	void OnMouseDown()
+	{
+		if (isQuit) 
+		{
+			Application.Quit();
+		}
+		else
+		{
+			Application.LoadLevel ("SurviveTheNight");
+		}
+	}
+}
